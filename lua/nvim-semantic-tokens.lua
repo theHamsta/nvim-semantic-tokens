3366ae152594120f485f83971d04e24c5de54915
Stephan Seitz <stephan.seitz@fau.de>
2022-02-14 14:35:29 +0100

    return on empty line when highlighting

--- lua/nvim-semantic-tokens.lua
+++ lua/nvim-semantic-tokens.lua
@@ -9,6 +9,7 @@ local function highlight(ctx, token, hl)
   local line_str = vim.api.nvim_buf_get_lines(ctx.bufnr, token.line, token.line + 1, false)[1]
   local start_byte = vim.lsp.util._str_byteindex_enc(line_str, token.start_char, token.offset_encoding)
   local end_byte = vim.lsp.util._str_byteindex_enc(line_str, token.start_char + token.length, token.offset_encoding)
+  if #line_str == 0 then return end
   vim.api.nvim_buf_set_extmark(ctx.bufnr, ns, token.line, start_byte, {
     end_row = token.line,
     end_col = math.min(end_byte, #line_str),
